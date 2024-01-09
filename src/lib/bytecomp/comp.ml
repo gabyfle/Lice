5b6068e02a1c20a6c5bcc3818670630a2c1edf9e
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2024-01-09 20:29:02 +0100

    [Compile] Adding some function skeleton
    Signed-off-by: Gabriel Santamaria <gaby.santamaria@outlook.fr>

--- src/lib/bytecomp/comp.ml
+++ src/lib/bytecomp/comp.ml
@@ -23,9 +23,35 @@
 open Ast
 open Types
 
-let expr_comp : Base.expr -> Opcode.t = function _ -> Opcode.empty
+let binop_comp : Base.binop_type -> Opcode.t = function _ -> Opcode.empty
 
-let stmt_comp : Tree.statement -> Opcode.t = function _ -> Opcode.empty
+let const_comp : Base.t -> Opcode.t = function _ -> Opcode.empty
+
+let var_comp : Base.value -> Opcode.t = function _ -> Opcode.empty
+
+let rec expr_comp : Base.expr -> Opcode.t = function
+  | Base.BinOp (op, left, right) ->
+      let left = expr_comp left in
+      let right = expr_comp right in
+      let op = binop_comp op in
+      left @ right @ op
+  | Base.Terminal (Base.Const c) ->
+      const_comp c
+  | Base.Terminal (Base.V_Var _ as v) ->
+      var_comp v
+  | _ ->
+      Opcode.empty (* TODO: Function call *)
+
+let stmt_comp : Tree.statement -> Opcode.t = function
+  | Tree.Return (_, e) ->
+      let e = expr_comp e in
+      e @ [Opcode.RET]
+  | Tree.Assign (_, v, e) ->
+      let e = expr_comp e in
+      let v = var_comp (Base.V_Var v) in
+      e @ v @ [Opcode.STORE (0, 0)]
+  | _ ->
+      Opcode.empty
 
 let bytecomp (prog : Tree.program) : Opcode.t =
   let rec aux (prog : Tree.program) : Opcode.t =
